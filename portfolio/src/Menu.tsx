3b7578accbbae6454e257be011a888a0ae9f8606
GregoireARichard <gregoire.richard@hetic.net>
2023-08-16 11:05:37 +0200

    fix: text p format

--- portfolio/src/Menu.tsx
+++ portfolio/src/Menu.tsx
@@ -4,7 +4,7 @@ import NormalView from "./NormalView";
 import SwaggerUI from "swagger-ui-react";
 import "./css/menu.css";
 
-const urlOpenApi = `${process.env.REACT_APP_API_URL}/openapi`
+const urlOpenApi = `${process.env.REACT_APP_API_URL}/openapi`;
 const Menu: React.FC = () => {
   const [view, setView] = useState<"swagger" | "normal">("normal");
 
@@ -27,9 +27,12 @@ const Menu: React.FC = () => {
         See the Normal UI
       </button>
       <h1>Hello 👋 I'm Grégoire, a Paris-based Back-end Engineer </h1>
-      <p>As you might've noticed, I'm not really much of a front-end dev... 😳 </p>
-      <p>You may use either the UI version 📱 or the API version ⚙️ (with Postman or any other HTTP Request engine) to learn about me. </p>
-      <p>You may call {process.env.REACT_APP_API_URL} if you're using the swagger API view.</p>
+      <p>
+        As you might've noticed, I'm not really much of a front-end dev... 😳 <br />
+        You may use either the UI version 📱 or the API version ⚙️ (with Postman <br />
+        or any other HTTP Request engine) to learn about me. You may call <br />
+        {process.env.REACT_APP_API_URL} if you're using the swagger API view.
+      </p>
       {view === "swagger" && <SwaggerUI url={urlOpenApi} />}
       {view === "normal" && <NormalView />}
     </div>
